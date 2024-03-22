d72d36ccbb9d2ad2b17de80952287c3d430f0277
aufall02 <aufaluzumaki@gmail.com>
2024-03-22 10:04:22 +0700

    chore : update code

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cv2 as cv
 
 def get_pixel(path):
-    print('\n*******************************************')
+    print('\n*********************************************')
     print('************* GET PIXE ON IMAGE *************')
     img = cv.imread(path)
     for i in range(10):
         k = img[0,i]
-        print('********************************************')
+        print('*********************************************')
         print('nilai rgb dari pixel ke : {0} \n red   : {1}\n green : {2}\n blue  : {0}'
             .format(i, k[2],k[1],k[0]))
 
 
 
 
-get_pixel('./img.png')
+get_pixel('./images/img.png')
